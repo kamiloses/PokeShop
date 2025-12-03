95aa786f6fd59b0402f58e265c22604ccdac2444
kamiloses <kamil.kurzaj04@gmail.com>
2025-12-03 10:04:15 +0100

    Implemented Header

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img src="/favicon.ico" alt="logo">
+    <nav>
+      <ul class="left">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Product</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+      <ul class="right">
+        <li><a href="#">Sign In</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  background-color: darkslategrey;
+  align-items: center;
+  color: white;
+  padding: 1.3rem;
+}
+nav{
+  display: flex;
+  flex: 1;  /*Todo Ogarnij*/
+  justify-content: space-between;
+}
+nav ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  gap:2rem; /* todo ogarnij */
+
+}
+
+a{color: white;
+  font-size: 1.2rem;
+text-decoration: none;
+}
+
+nav ul.left{
+margin-left: 2rem;
+}
+
+nav ul.right {
+margin-right: 3rem;
+}
+
+</style>
